a1f3b9b8094edcc3d18852b8b3d2265974371924
Lancelot Gordon <lance032017@gmail.com>
2024-08-15 22:16:53 -0400

    integrate kambi widget

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -6,15 +6,17 @@ import TopNav from './components/TopNav';
 import Promotions from './components/Promotions';
 import Leagues from './components/Leagues';
 import SpecialsList from './components/SpecialsList';
+import KambiBC from './components/KambiBC';
 
 export default function HomeScreen() {
   return (
-    <div className="home-container dark">
+    <div className="homescreen-container dark">
       <TopNav />
       <Promotions />
       <Leagues>
         <SpecialsList />
       </Leagues>
+      <KambiBC />
     </div>
   )
 };
---
+++ src/components/KambiBC.js
@@ -0,0 +1,68 @@
+import React, { useEffect, useState } from 'react';
+
+export default function KambiBC() {
+
+  useEffect(() => {
+    (function (global) {
+      var CustomerSettings = function() {
+        this.enableNavigationPanel = true;
+      };
+      global.customerSettings = new CustomerSettings();
+    })(window);
+    window._kc = {
+      // Inject start-up parameters here when serving the page
+      currency: 'USD',
+      locale: 'en_GB',
+      market: 'MT',
+      playerId: '',
+      ticket: '',
+      oddsFormat: 'fractional',
+      racingMode: 'false',
+      environment: 'ctn'
+    };
+  }, []);
+
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "https://client-static.bc.ctn.kambicdn.com/client/integration3/kambi-bootstrap.js";
+    script.async = true;
+    document.body.appendChild(script);
+    return () => {
+      document.body.removeChild(script);
+    }
+  }, []);
+
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "https://kambi-widget-api.bc.ctn.kambicdn.com/kambi-widget-api.js";
+    script.async = true;
+    script.onload = () => {
+      handleKambiWidget();
+    };
+    document.body.appendChild(script);
+    return () => {
+      document.body.removeChild(script);
+    }
+  }, []);
+
+  const handleKambiWidget = () => {
+    window.KambiWidget.ready.then(function (wapi) {
+      window.wapi = wapi;
+
+      wapi.subscribe((response, wapi) => {
+        switch (response.type) {
+          case wapi.WIDGET_HEIGHT:
+            console.log('Current height is: ', response.data);
+          break;
+          /* ... */
+        }
+      });
+    });
+  };
+
+  return (
+    <div className="hidden">
+      <div id="KambiBC"></div>
+    </div>
+  )
+}
