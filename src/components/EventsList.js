605145938738442c8f1686a2a60fd32b3a0a0d35
Lancelot Gordon <lance032017@gmail.com>
2024-08-30 21:26:06 -0400

    filter out 'baseball team - x' from eventlist

--- src/components/EventsList.js
+++ src/components/EventsList.js
@@ -18,6 +18,7 @@ export default function EventsList({league}) {
         response.data.events
           ?.filter(item => !league.groupMatches || item.event.group === league.group)
           .filter(item => moment(item.event.start).diff(moment(), 'days') > -1)
+          .filter(item => !item.event.englishName.includes('Baseball Team'))
           .map(item => ({...item, score: item.liveData?.score}))
           || []
       );
