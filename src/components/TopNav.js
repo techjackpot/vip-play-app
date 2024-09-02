1a39f626c7ebbf37816f310df75d4960ee386741
Lancelot Gordon <lance032017@gmail.com>
2024-09-01 23:07:43 -0400

    add search popup

--- src/components/TopNav.js
+++ src/components/TopNav.js
@@ -1,5 +1,113 @@
 import React, { useEffect, useState } from 'react';
 
+const searchIcon = "https://res.cloudinary.com/production/image/upload/v1723572404/Icons/VIP/search.svg";
+
+function Search({active, setSearchActive}) {
+  const  navLinks = [
+    {
+      name: 'live',
+      label: 'Live Now',
+      icon: '',
+    },
+    {
+      name: 'promos',
+      label: 'Promos',
+      icon: 'https://res.cloudinary.com/production/image/upload/v1723572404/Icons/VIP/promos.svg',
+    },
+    {
+      name: 'boosts',
+      label: 'Boosts',
+      icon: 'https://res.cloudinary.com/production/image/upload/v1723572405/Icons/VIP/boosts.svg',
+    },
+    {
+      name: 'mlb',
+      label: 'MLB',
+      icon: 'https://res.cloudinary.com/production/image/upload/v1723572404/Icons/VIP/mlb.svg',
+    },
+    {
+      name: 'nfl',
+      label: 'NFL',
+      icon: '',
+    },
+    {
+      name: 'wnba',
+      label: 'WNBA',
+      icon: '',
+    },
+    {
+      name: 'tennis',
+      label: 'Tennis',
+      icon: '',
+    },
+    {
+      name: 'soccer',
+      label: 'Soccer',
+      icon: '',
+    },
+    {
+      name: 'golf',
+      label: 'Golf',
+      icon: '',
+    },
+    {
+      name: 'nhl',
+      label: 'NHL',
+      icon: '',
+    },
+    {
+      name: 'nba',
+      label: 'NBA',
+      icon: '',
+    },
+    {
+      name: 'ufc',
+      label: 'UFC',
+      icon: '',
+    },
+    {
+      name: 'ncaa_football',
+      label: 'NCAA Football',
+      icon: '',
+    },
+    {
+      name: 'ncaa_basketball',
+      label: 'NCAA Basketball',
+      icon: '',
+    },
+  ];
+  const sports = [];
+  return (
+    <div className={`search-container ${!active ? 'closed' : ''}`}>
+      <div className="search-header">
+        <a className="go-back text-orange" onClick={(e) => setSearchActive(false)}>&lt; Back</a>
+        <strong>Search</strong>
+        <a className="close-search text-orange" onClick={(e) => setSearchActive(false)}>Close</a>
+      </div>
+      <div className="search-bar">
+        <img src={searchIcon} alt="" />
+        <input className="search-input" type="search" placeholder="Search" />
+      </div>
+      <div className="search-content-type">
+        <button className={`btn-search-type`}>All Sports</button>
+      </div>
+      <div className="search-content">
+        <ul className="main-nav-links">
+          {navLinks.map(item => (
+            <li className="nav-link-li">
+              <a className="main-nav-link">
+                <div className="main-nav-link-prefix">
+                  {item.icon && <img src={item.icon} alt="" />}
+                </div>
+                {item.label}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  )
+}
+
 function TopNavLink({active, item, onClick}) {
   return (
     <a href="/" className={`topnav-link ${active && !item.cannot_active ? 'active' : ''}`} onClick={(e) => onClick(e, item)}>
@@ -16,6 +124,7 @@ export default function TopNav() {
       label: 'All',
       icon: 'https://res.cloudinary.com/production/image/upload/v1723572404/Icons/VIP/search.svg',
       cannot_active: true,
+      is_search: true,
     },
     {
       name: 'live',
@@ -54,10 +163,14 @@ export default function TopNav() {
     },
   ];
   const [activeNav, setActiveNav] = useState(null);
+  const [isSearchActive, setSearchActive] = useState(false);
 
   const onClick = (e, item) => {
     e.preventDefault();
     setActiveNav(item.name);
+    if (item.is_search) {
+      setSearchActive(true);
+    }
     return false;
   };
 
@@ -68,6 +181,7 @@ export default function TopNav() {
           <TopNavLink active={activeNav === item.name} item={item} key={item.name} onClick={onClick} />
         ))}
       </div>
+      <Search active={isSearchActive} setSearchActive={setSearchActive} />
     </div>
   )
 }
